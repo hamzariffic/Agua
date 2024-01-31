fb48be2e3b8e4c10bf39910baa8f45bd7df02225
hamzariffic <hamza.mattea@gmail.com>
2024-01-31 12:12:43 +0300

    minimal debugs

--- app/src/main/java/com/example/agua/Network/APIInstance.kt
+++ app/src/main/java/com/example/agua/Network/APIInstance.kt
@@ -1,5 +1,6 @@
 package com.example.agua.Network
 
+import retrofit.GsonConverterFactory
 import retrofit2.Retrofit
 
 const val BASE_URL = "/rapidapi.com/"
@@ -10,7 +11,14 @@ object APIInstance {
 }
 private fun addGsonConverter(): Retrofit.Builder {
     return Retrofit.Builder().baseUrl(BASE_URL)
-        .addConverterFactory(addGsonConverter.create())
+        .addConverterFactory(GsonConverterFactory.create())
+}
+
+
+//Clean up this code and remove duplicate classes
+private fun Retrofit.Builder.addConverterFactory(create: GsonConverterFactory?): Retrofit.Builder {
+    TODO("Not yet implemented")
+
 }
 
 
